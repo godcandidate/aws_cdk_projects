571461d05fcdfbc777d38098d8d1f5c48054ea27
godcandidate <godcandidate101@gmail.com>
2025-01-16 14:41:17 +0000

    Initial commit

---
+++ tests/unit/test_portfolio_cdk_app_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from portfolio_cdk_app.portfolio_cdk_app_stack import PortfolioCdkAppStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in portfolio_cdk_app/portfolio_cdk_app_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = PortfolioCdkAppStack(app, "portfolio-cdk-app")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
